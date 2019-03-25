89c8cc06c0ea73b1cb9f6b433f501e56dabc57fa
Ben Forleo <brz23@wildcats.unh.edu>
2019-03-25 08:38:23 -0400

    using doc2vec
    After much expirimentation, it appears that I have stumbled upon some signifigant insights using doc2vec, t-SNE and KMeans. More to come...

--- processing_functions.py
+++ processing_functions.py
@@ -43,11 +43,68 @@ def is_account_pipe(doc):
     
     return doc
 
-# %%
-
-## remove weblinks and RT symbol
+# %% clean and lemmatise each document doc
 
 def clean_doc(doc):
+    
+    from spacy.lang.en.stop_words import STOP_WORDS
+    import re
+    import string
+    
+    more_stops = ['i', 'it', 'him', 'they', 'he', 'her', 'its', 'be', 'the', 'you',
+                 'not', 'we', 'a', 'me', 'like', 'his', 'this']
+    pattern = re.compile('\.+')
+    
+    token_list = []
+    
+    for token in doc:
+        if token._.is_account or token._.is_hashtag:
+            token_list.append(token.text)
+            
+        elif (token.lemma_ != "-PRON-") and (not token.is_punct) and (not token.is_space) \
+        and (not bool(re.fullmatch(pattern, token.lemma_)))\
+        and (not token.is_stop):
+            
+            token_list.append(token.lemma_.lower().strip())
+            
+        elif token.lemma_ == "-PRON-":
+            
+            token_list.append(token.lower_)
+        
+    
+    token_list = [token for token in token_list if token not in more_stops]
+            
+    
+    return token_list
+
+
+# %% clean doc with no lemmatization
+
+def clean_doc_no_lemma(doc):
+    
+    import re
+    import string
+    punctuations = string.punctuation
+    
+    token_list = []
+    
+    for token in doc:
+        if token._.is_account or token._.is_hashtag:
+            token_list.append(token.text)
+            
+        elif (token.text.strip() not in punctuations) and (token.text.strip != ""):
+            
+            token_list.append(token.lower_)
+            
+    return token_list
+
+
+
+
+# %%
+## remove weblinks and RT symbol
+
+def clean_doc_old(doc):
     
     """Removes hyperlinks, the RT indicator, punctuation and stop words. 
     Returns lowercase lemmatized words"""
@@ -64,14 +121,68 @@ def clean_doc(doc):
         if token._.is_account or token._.is_hashtag:
             token_list.append(token.text)
   
-        elif (token.text != "RT") and not(token.text in STOP_WORDS) \
+        elif (token.text != "RT") \
+        and not(token.text in STOP_WORDS) \
         and not bool(re.match(link_pattern, token.text)) \
         and not(token.is_punct):
-            token_list.append(token.lemma_)
+            token_list.append(token.lower_)
      
     return token_list
 
 #%%
+
+def rt_remover(doc):
+    
+    import re
+    
+    rt_pattern = re.compile("RT ")
+    
+    less_rt = re.sub(rt_pattern, "", doc)
+        
+    return less_rt
+
+# %%
+
+def link_remover(doc):
+    
+    import re
+    
+    link_pattern = re.compile(
+        "(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?")
+    
+    less_link = re.sub(link_pattern, "", doc)
+    
+    return less_link
+
+# %%
+
+def lower_case(doc):
+    
+    """Returns the lowercase form of a token"""
+    
+    token_list = []
+    
+    for token in doc:
+        
+        token_list.append(token.lower_)
+        
+    return token_list
+
+# %% flattens a lists of lists into a single list
+
+def group_lists(l):
+    
+    return [item for sublist in l for item in sublist]
+    
+    
+        
+        
+
+        
+        
+        
+    
+    
 
 
 
